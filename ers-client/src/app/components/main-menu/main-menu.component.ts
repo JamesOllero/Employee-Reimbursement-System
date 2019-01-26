4c4b0ce4dac1c57845f682b01c419e68b82ceda6
James D Ollero <James.D.Ollero@outlook.com>
2019-01-25 19:31:41 -0500

    copied more login-Demo stuff

--- ers-client/src/app/components/main-menu/main-menu.component.ts
+++ ers-client/src/app/components/main-menu/main-menu.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
 })
-export class MainMenuComponent implements OnInit {
 
-  constructor() { }
+export class MainMenuComponent implements OnInit {
+  userNavs: Array<{title: string}>;
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+      if (this.authService.hasRoles(['Manager'])) {
+          this.userNavs = environment.managerNav;
+      } else {
+          this.userNavs = environment.employeeNav;
+      }
   }
-
 }
+
