9e5e218de47283bcd7d69d04f5fc6090ab97ff9f
Chris Oberg <oberg.chris@yahoo.com>
2019-01-30 20:14:27 -0500

    Angular: Added main menu, Began configuration of app.module

--- ers-client/src/app/app.module.ts
+++ ers-client/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRouterModule } from './app-router.module';
+import { FormsModule } from "@angular/forms";
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
